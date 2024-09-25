a99a6a38a9a3384c3829b1d5c1512296d957bdb1
Norbilouz <ilmulangit114@gmail.com>
2024-09-25 13:59:55 +0700

    nambah speed

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function toggleFan() {
     toggleButton.textContent = 'Turn On Fan'; // Ubah teks tombol
   } else {
     // Hidupkan kipas
-    intervalId = setInterval(animateFan, 100); // Jalankan animasi setiap 100ms
+    intervalId = setInterval(animateFan, 100); // Jalankan animasi setiap 2 0ms
     toggleButton.textContent = 'Turn Off Fan'; // Ubah teks tombol
   }
   isFanOn = !isFanOn; // Ubah status kipas
